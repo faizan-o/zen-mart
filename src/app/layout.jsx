ece728198262cca89c1372dec2e2462ff331ed80
faizan-o <roocking.prince@gmail.com>
2024-04-05 19:15:33 +0500

    Modified Home Page SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,10 @@
 import { Navbar, Footer } from '../components/export';
 import './globals.css';
 
-export const metadata = {
-  title: 'Zenmart | Buy The Best Products',
-  description: 'Zenmart is your go-to destination for premium online shopping, offering a diverse range of high-quality products to meet your every need. Whether you are in search of cutting-edge electronics or the latest fashion trends, Zenmart has something for everyone. Our electronics department features top-of-the-line smartphones, laptops, cameras, and accessories from renowned brands, ensuring you stay ahead in the tech game. Explore our fashion collection for men, women, and children, where you will discover stylish apparel, footwear, and accessories to elevate your wardrobe. With Zenmart, you can shop with confidence, knowing that each product is carefully curated for its quality and value. Experience seamless browsing and hassle-free purchasing, backed by our commitment to exceptional customer service. Join us at Zenmart and embark on a journey of unparalleled convenience and satisfaction in online shopping.',
-}
+// export const metadata = {
+//   title: 'Zenmart | Buy The Best Products',
+//   description: 'Zenmart is your go-to destination for premium online shopping, offering a diverse range of high-quality products to meet your every need. Whether you are in search of cutting-edge electronics or the latest fashion trends, Zenmart has something for everyone. Our electronics department features top-of-the-line smartphones, laptops, cameras, and accessories from renowned brands, ensuring you stay ahead in the tech game. Explore our fashion collection for men, women, and children, where you will discover stylish apparel, footwear, and accessories to elevate your wardrobe. With Zenmart, you can shop with confidence, knowing that each product is carefully curated for its quality and value. Experience seamless browsing and hassle-free purchasing, backed by our commitment to exceptional customer service. Join us at Zenmart and embark on a journey of unparalleled convenience and satisfaction in online shopping.',
+// }
 
 export default function RootLayout({ children }) {
   return (
